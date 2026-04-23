123e9018395695e46f2e34e20c37984179845a2a
Jelly <skguswl8038@naver.com>
2026-04-23 22:40:52 +0900

    first commit

---
+++ src/views/login/ui/LoginPage.tsx
@@ -0,0 +1,10 @@
+import { authApi } from '@/src/features/auth'
+
+export function LoginPage() {
+  return (
+    <main>
+      <button onClick={authApi.kakaoLogin}>카카오로 로그인</button>
+      <button onClick={authApi.googleLogin}>구글로 로그인</button>
+    </main>
+  )
+}
