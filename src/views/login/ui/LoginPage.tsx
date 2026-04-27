e9654924bcf264e2f581a5171e833d773cd93646
Jelly <skguswl8038@naver.com>
2026-04-27 21:32:26 +0900

    feat: 로그인 화면 추가
    - 필요 없는 이미지 삭제
    - 이미지 위치 public으로 이동
    - 구글 버튼 컴포넌트
    - 마스코트 이미지 추가

--- src/views/login/ui/LoginPage.tsx
+++ src/views/login/ui/LoginPage.tsx
@@ -1,10 +1,35 @@
-import { authApi } from '@/src/features/auth';
+'use client';
+
+import GoogleLoginButton from '@/features/auth/ui/GoogleLoginButton';
+
+const onGoogle = () => {};
 
 export function LoginPage() {
   return (
-    <main>
-      <button onClick={authApi.kakaoLogin}>카카오로 로그인</button>
-      <button onClick={authApi.googleLogin}>구글로 로그인</button>
+    <main className="flex-1 max-w-screen-xl mx-auto w-full px-4 pb-20 lg:pb-0">
+      <div className="w-full max-w-sm mx-auto bg-white p-8">
+        <div className="mx-auto mb-4 max-w-[6rem]">
+          <img src="/images/logo/logo.svg" alt="corea hoy" />
+        </div>
+
+        <p className="text-center text-gray-500 text-sm mb-[4rem] leading-relaxed">
+          요즘, 한국에는 무슨 일이 있을까?
+          <br />
+          로그인하고 소식을 더욱 빠르게 만나보세요
+        </p>
+
+        <form>
+          <div className="relative">
+            <GoogleLoginButton onClick={onGoogle} />
+            <div className="absolute right-[-1rem] top-[-3.5rem] w-[5rem]">
+              <img src="/images/characters/mascot-v.svg" alt="" />
+            </div>
+          </div>
+          <p className="mt-5 text-[11px] leading-relaxed text-gray-400 text-center px-2">
+            계속 진행하면 코레아 호이의 이용약관 및 개인정보 처리방침에 동의하는 것으로 간주됩니다.
+          </p>
+        </form>
+      </div>
     </main>
   );
 }
