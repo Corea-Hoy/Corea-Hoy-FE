b35cc88b01be4e99ba37135d473c2bae24f7e8cd
insu1170 <insu1170@naver.com>
2026-05-06 14:09:27 +0900

    feat: Suspense 적용

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,3 +1,10 @@
+import { Suspense } from 'react';
 import { AdminPipelinePage } from '@/views/admin-pipeline';
 
-export default AdminPipelinePage;
+export default function Page() {
+  return (
+    <Suspense>
+      <AdminPipelinePage />
+    </Suspense>
+  );
+}
