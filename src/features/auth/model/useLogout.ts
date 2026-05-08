2ea2b067ae5f29b655b59028948b98ff25c927a5
insu1170 <insu1170@naver.com>
2026-05-08 18:23:22 +0900

    fix: logout기능을 위한 React.Query 수정

--- src/features/auth/model/useLogout.ts
+++ src/features/auth/model/useLogout.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/navigation';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logout } from '@/features/auth/api/auth.api';
 import { useUsersStore } from '@/entities/user/model/user.store';
 
@@ -7,14 +7,14 @@ export const useLogout = () => {
   const router = useRouter();
   const storeLogout = useUsersStore((state) => state.logout);
 
+  const queryClient = useQueryClient();
+
   const { mutate: onLogout, isPending } = useMutation({
     mutationFn: logout,
-    onSuccess: () => {
+    onSettled: () => {
+      queryClient.removeQueries({ queryKey: ['me'] });
       storeLogout();
       router.push('/login');
-    },
-    onError: (error) => {
-      console.error('Logout failed:', error);
     },
   });
 
