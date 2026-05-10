a3632a47cf72de8182575df734af7ca1264b954a
insu1170 <insu1170@naver.com>
2026-05-10 19:44:44 +0900

    feat: 커스텀 alert 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 export { Chip } from './chip/Chip';
 export { Stepper } from './stepper/Stepper';
 export { ConfirmModal } from './modal/ConfirmModal';
+export { AlertModal } from './modal/AlertModal';
 export { Loading } from './loading/Loading';
 export { Avatar } from './avatar/Avatar';
