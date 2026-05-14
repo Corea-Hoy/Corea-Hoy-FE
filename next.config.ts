3d86e6d336a062ca226579d6c17353499c64260a
Jelly <skguswl8038@naver.com>
2026-05-14 21:20:21 +0900

    fix: 로그인 기능 수정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
